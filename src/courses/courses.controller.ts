bcd41dd904ce5a7661562cba32b50cda25fd2891
Fransualdo Lopes <fransualdo.14@gmail.com>
2024-04-21 10:15:56 -0300

    subindo repo

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, HttpCode, Param, Post, Res } from '@nestjs/common';
+import path from 'path';
+
+@Controller('courses')
+export class CoursesController {
+    @Get()
+    findAll(@Res() response) {
+        return response.status(200).json({message:'Listagem de cursos'});
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return `Curso com ID ${id}`;
+    }
+    @HttpCode(204)
+    @Post()
+    create(@Body() body) {
+        return body;
+    }
+}
+
